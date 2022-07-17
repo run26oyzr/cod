690a1037eb66e04f8ca518aa68bbb071f19a90db
欧阳宗润 <ouyangzongrun@qq.com>
2022-07-17 21:47:09 +0800

    P2234 AC

--- stl/P2234.cpp
+++ stl/P2234.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 struct Node{
     int id, val;
     bool operator < (const Node T)const{
         // cout << "T.val=" << T.val << "; val=" << val << endl;
-        if(val == T.val) reuturn id < T.id;
+        if(val == T.val) return id < T.id;
         return val < T.val;
     }
 };
 set <Node> s;
 set <Node>::iterator it;
-int main(){
+signed main(){
     int t;
     cin >> t;
     Node n;
@@ -22,14 +23,15 @@ int main(){
     n.val = val;
     n.id = 1;
     s.insert(n);
-    t--;
     for(int j = 2; j <= t; j++){
-        int delta = 1e9 + 5;
+        int delta = 1e18 + 5;
         cin >> val;
+        // cout << val << endl;
         n.id = j;
         n.val = val;
         s.insert(n);
         it = s.lower_bound(n);
+        int ans = 1e9;
         if(it != s.begin()){
             it--;
             // cout << "lower " << (*it).val << endl;
@@ -40,20 +42,22 @@ int main(){
         }
         it = s.upper_bound(n);
         if(it != s.end()){
-            if ((*--it).val == val &&(*it).id != j){
-                continue;
-            }
-            else it++;
+            // if ((*--it).val == val &&(*it).id != j){
+            //     continue;
+            // }
+            // else it++;
             // cout << "upper " << (*it).val << endl;
             if (delta > (*it).val - n.val){
                 delta = (*it).val - n.val;
-                ans = delta;
+                // cout << delta << endl;
+                ans = min(ans, delta);
             }
         }
-        for(it = s.begin();it!=s.end();it++){
+        // cout << "ans " << ans << endl; 
+        // for(it = s.begin();it!=s.end();it++){
             // cout << "id=" << (*it).id <<"; val = " << (*it).val << endl; 
-        }
-        cout << " ------------------------------------------ans="<< ans << endl;
+        // }
+        // cout << " ------------------------------------------ans="<< ans << endl;
         res += ans;
     }
     cout << res;
