74a509428188486ac6ba2f9a2f9235724c64794a
oyzr <ouyangzongrun@qq.com>
2022-07-30 19:17:44 +0800

    P2278 AC

--- stl/P2278.cpp
+++ stl/P2278.cpp
@@ -1,15 +1,44 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 struct Node{
-    int id, val, start;
+    int priority_level, num, start, lasts;
     bool operator < (const Node T) const{
-        if (T.id == id) return T.start < start;
-        return T.id > id;
+        if (T.priority_level == priority_level) return T.num < num;
+        return T.priority_level > priority_level;
     }
 };
 priority_queue<Node> q;
-int main(){
-    
+signed main(){
+    Node now, nt;
+    cin >> now.num >> now.start >> now.lasts >> now.priority_level;
+    q.push(now);
+    int nowtime = now.start;
+    while(cin >> now.num){
+        cin >> now.start >> now.lasts >> now.priority_level;
+        // scanf("%lld %lld %lld", &now.start, &now.lasts, &now.priority_level);
+        nt = q.top(); q.pop();
+        int x = now.start - nowtime;
+        while (nt.lasts <= x){
+            cout << nt.num << ' ' << nowtime + nt.lasts << endl;
+            nowtime += nt.lasts;
+            nt.lasts = 0;
+            if (q.empty()) break;
+            nt = q.top(); q.pop();
+            x = now.start - nowtime;
+        }
+        if (nt.lasts != 0){
+            nt.lasts -= x;
+            q.push(nt);
+        }
+        nowtime = now.start;
+        q.push(now);
+    }
+    while(!q.empty()){
+        nt = q.top(); q.pop();
+        cout << nt.num << ' ' << nowtime + nt.lasts << endl;
+        nowtime += nt.lasts;
+    }
     system("Pause");
     return 0;
 }
