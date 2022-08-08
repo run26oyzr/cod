3f9978a586422bb6a7e046d1513c34484531939f
oyzr <ouyangzongrun@qq.com>
2022-08-08 20:02:23 +0800

    P1972 AC

--- data_structure/segment_tree_2/P1972.cpp
+++ data_structure/segment_tree_2/P1972.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
-const int maxn = 1e6;
-int n, m, maxm = 1;
+const int maxn = 2 * 1e6 + 5;
+int n, m;
 struct node{
-    int x, y;
+    int x, y, id;
     bool operator < (const node T) const {
-        return T.y < y;
+        return y < T.y;
     }
 }ask[maxn];
+int ans[maxn];
 int tree[maxn], t[maxn];
 int lastpos[maxn];
 int lowbit(int x) {return x & (-x);}
 void add(int id, int val){
+    // if (id == 0) return;
     while(id <= n){
         tree[id] += val;
         id += lowbit(id);
@@ -25,30 +28,33 @@ int sum(int id){
     }
     return res;
 }
-int main(){
+signed main(){
     cin >> n;
     for (int i = 1; i <= n; i++){
         cin >> t[i];
-        add(i, 1);
-        if (lastpos[t[i]] == 0) lastpos[t[i]] = i;
+        // add(t[i], 1);
+        // if (lastpos[t[i]] == 0) lastpos[t[i]] = i;
     }
     cin >> m;
     int x, y;
     for (int i = 1; i <= m; i++){
         cin >> ask[i].x >> ask[i].y;
+        ask[i].id = i;
     }
     sort(ask + 1, ask + m + 1);
+    int left = 1;
     for (int j = 1; j <= m; j++){
         x = ask[j].x; y = ask[j].y;
-        if (y > maxm){
-            for (int i = maxm + 1; i <= y; i++){
-                add(lastpos[t[i]], -1);
-                lastpos[t[i]] = i;
-                add(i, 1);
-            }
-            maxm = y;
+        for (int i = left; i <= y; i++){
+            if (lastpos[t[i]]) add(lastpos[t[i]], -1);
+            add(i, 1);
+            lastpos[t[i]] = i;
         }
-        cout << sum(y) - sum(x - 1) << endl;
+        left = y + 1;
+        ans[ask[j].id] = sum(y) - sum(x - 1);
+    }
+    for (int i = 1; i <= m; i++){
+        cout << ans[i] << endl;
     }
     system("Pause");
     return 0;
