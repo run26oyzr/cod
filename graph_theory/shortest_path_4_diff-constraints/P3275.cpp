0af73b572199f3d6f07352b9893c105f5040333b
oyzr <ouyangzongrun@qq.com>
2022-08-21 11:07:59 +0800

    P3275 unaccepted

---
+++ graph_theory/shortest_path_4_diff-constraints/P3275.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e4 + 5;
+int h[maxn], tot = 0;
+int n, m;
+struct node{
+    int v, c, next;
+}t[maxn << 2];
+void addEdge(int u, int v, int c){
+	++tot;
+	t[tot].c = c; t[tot].v = v; t[tot].next = h[u]; h[u] = tot;
+}
+int s, dis[maxn], vis[maxn], num[maxn];
+queue<int> q;
+bool spfa(int x){
+    s = x;
+	while(!q.empty()) q.pop();
+	memset(dis, -1, sizeof(dis));
+	memset(vis, 0, sizeof(vis));
+	dis[s] = 0; q.push(s); num[s] = 1; vis[s] = 1;
+	while(!q.empty()){
+		int u = q.front(); q.pop(); vis[u] = 0;
+		for (int i = h[u]; i; i = t[i].next){
+			int v = t[i].v;
+			if (dis[v] < dis[u] + t[i].c){
+				dis[v] = dis[u] + t[i].c;
+				if (!vis[v]){
+					q.push(v); vis[v] = 1; num[v] ++;
+					if(num[v] > n) return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+int main(){
+    tot = 0;
+    cin >> n >> m;
+    int x, y, z;
+    for (int i = 1; i <= m; i++){
+        cin >> x >> y >> z;
+        if (x == 1){
+            addEdge(y, z, 0);
+            addEdge(z, y, 0);
+        }
+        if (x == 2){
+            addEdge(y, z, 1);
+        }
+        if (x == 3){
+            addEdge(z, y, 0);
+        }
+        if (x == 4){
+            addEdge(z, y, 1);
+        }
+        if (x == 5){
+            addEdge(y, z, 0);
+        }
+    }
+    n++;
+    for (int i = 1; i <= n; i++){
+        addEdge(n, i, 1);
+    }
+    if (spfa(n)) cout << -1;
+    else{
+        int ans = 0;
+        for (int i = 1; i < n; i++){
+            ans += dis[i];
+        }
+        cout << ans;
+    }
+    system("pause");
+    return 0;
+}
