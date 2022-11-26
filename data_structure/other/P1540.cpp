4910c67f5524074a86d032e73028f6b8f799d412
oyzr <ouyangzongrun@qq.com>
2022-11-26 18:37:32 +0800

    P1540 AC

--- data_structure/other/P1540.cpp
+++ data_structure/other/P1540.cpp
@@ -7,8 +7,16 @@ int main(){
     int m, n;
     cin >> m >> n;
     int x;
+    int cnt = 0;
     while(n--){
         cin >> x;
+        if (vis[x]) continue;
+        cnt++;
+        q.push(x);
+        vis[x] = 1;
+        if (q.size() > m) {vis[q.front()] = 0; q.pop();}
     }
+    cout << cnt;
+    system("pause");
     return 0;
 }
