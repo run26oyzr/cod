5bb92b63a648fade7c36e98fbbfcba6eefd6dcd2
oyzr <ouyangzongrun@qq.com>
2022-11-26 17:48:35 +0800

    P1540 unfinished

---
+++ data_structure/other/P1540.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1005;
+int vis[maxn];
+queue <int> q;
+int main(){
+    int m, n;
+    cin >> m >> n;
+    int x;
+    while(n--){
+        cin >> x;
+    }
+    return 0;
+}
