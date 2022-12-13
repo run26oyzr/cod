b1a3d64b2ac435f875d8f3250fb826f73c8b89f1
oyzr <ouyangzongrun@qq.com>
2022-12-13 17:45:35 +0800

    模板加上快读

--- template.cpp
+++ template.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+template<typename T> inline void read(T &x) {
+	x=0;
+	char c=getchar();
+	for (;!isdigit(c);c=getchar());
+	for (;isdigit(c);c=getchar()) x=(x<<1)+(x<<3)+(c^48);
+}
 int main(){
 
     system("pause");
