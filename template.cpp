bf9f6dcaf29a06cad51d98264606b2a30b8c22ac
oyzr <ouyangzongrun@qq.com>
2022-12-16 15:27:32 +0800

    原来的快读不会用，先换一个

--- template.cpp
+++ template.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-template<typename T> inline void read(T &x) {
-	x = 0;
-	char c = getchar();
-	for (;!isdigit(c); c = getchar());
-	for (;isdigit(c); c = getchar()) x = (x << 1) + (x << 3) + (c ^ 48);
+// template<typename T> inline void read(T &x) {
+// 	x = 0;
+// 	char c = getchar();
+// 	for (;!isdigit(c); c = getchar());
+// 	for (;isdigit(c); c = getchar()) x = (x << 1) + (x << 3) + (c ^ 48);
+// }
+inline int read()
+{
+    register int x = 0,f = 1;
+    char c = getchar();
+    while(c < '0' || c > '9')
+    {
+        if(c == '-') f = -1;
+        c = getchar();
+    }
+    while(c >= '0' && c <= '9') 
+    {
+        x = (x << 3) + (x << 1) + (c ^ 48);
+        c = getchar();
+    }
+    return x * f;
 }
 int main(){
 
