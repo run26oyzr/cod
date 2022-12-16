022072ed2fb0b16d975ea48c822d1f0ebad35092
oyzr <ouyangzongrun@qq.com>
2022-12-16 13:53:51 +0800

    B 45pts

--- other's_competition/roud_four(CYYAKI)/B.cpp
+++ other's_competition/roud_four(CYYAKI)/B.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 const int maxn = 2 * 1e5 + 5;
 typedef long long ll;
@@ -45,9 +46,9 @@ void dijkstra(){
         }
     }
 }
-int main(){
-    freopen("treelove.in", "r", stdin);
-    freopen("treelove.out", "w", stdout);
+set <int> s1;
+set <int> ::iterator it;
+signed main(){
     srand(time(0));
     int n = read();
     for (int i = 1; i <= n; i++){
@@ -61,7 +62,6 @@ int main(){
     }
     dijkstra();
     //从源点算到各个点的异或值，两个点的异或值再异或就消掉上面的部分了
-    int ans = -1;
     /*
     //已知一个序列求任选两个数异或的最大值不会O(n),只会暴力……所以后面就是随机化的了……
     if (n <= 1e4){
@@ -80,9 +80,23 @@ int main(){
         }
     }
     */
+    /*
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= n; j++){
             ans = max(ans, dis[i] ^ dis[j]);
+        }
+    }
+    */
+    long long ans = 0, tot = 0;
+    for (int i = 32; i >= 0; i--){
+        s1.clear();
+        tot |= (1 << i);
+        for (int j = 1; j <= n; j++) s1.insert(tot & dis[j]);
+        for (it = s1.begin(); it != s1.end(); it++){
+            if (s1.count(ans + (1 << i) ^ (*it))){
+                ans += (1 << i);
+                break;
+            }
         }
     }
     cout << ans;
