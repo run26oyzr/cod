782b0f9f96532e8ded2046ea8fbe368c71fa509c
oyzr1234 <ouyangzongrun@qq.com>
2023-01-10 18:44:50 +0800

    LCA Tarjan 过样例，全RE

--- graph_theory/LCA/template(tarjan).cpp
+++ graph_theory/LCA/template(tarjan).cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e5 + 5, maxk = 25;
+const int maxn = 5 * 1e5 + 5;
 struct Node{
     int v, nxt, idx;
 }t[maxn << 1], tq[maxn << 1];
-int tot, h[maxn];
+int tot, h[maxn << 1];
 void addEdge(int u, int v){
     ++tot; 
     t[tot].v = v; t[tot].nxt = h[u]; h[u] = tot;
 }
-int totq, hq[maxn];
+int totq, hq[maxn << 1];
 void addQ(int u, int v, int idx){
     ++totq; 
     tq[totq].v = v; tq[totq].nxt = hq[u]; hq[u] = totq;
-    tq[totq].idx = totq;
+    tq[totq].idx = idx;
 }
-int fa[maxn];
-int vis[maxn];
-int ans[maxn];
+int fa[maxn << 1];
+int vis[maxn << 1];
+int ans[maxn << 1];
 int getfa(int x){
     if (fa[x] == x) return x;
     return fa[x] = getfa(fa[x]);
@@ -26,25 +26,34 @@ void merge(int x, int y){
     int fx = getfa(x);
     fa[getfa(y)] = fx;
 }
-void dfs(int u, int fa){
+void dfs(int u, int father){
+    cout << u << endl;
+    vis[u] = 1;
     for (int i = h[u]; i; i = t[i].nxt){
         int v = t[i].v;
-        if(v == fa) continue;
+        if (v == father) continue;
+        if (vis[v]) continue;
         dfs(v, u);
         merge(u, v); //只能u作为代表员
     }
     for (int i = hq[u]; i; i = tq[i].nxt){
         int v = tq[i].v;
-        if (vis[v]) ans[tq[i].idx] = getfa(v);
+        // cout << u << ' ' << v << endl;
+        // if (vis[v] && !ans[tq[i].idx]) cout << u << ' ' << v << ' ' << getfa(v) << endl;
+        // cout << ((vis[v])?100:99) << endl;
+        if (vis[v] && !ans[tq[i].idx]) ans[tq[i].idx] = getfa(v);
     }
 }
 int main(){
+    freopen("P3379_1.in", "r", stdin);
+    freopen("P3379.out", "w", stdout);
     int n, m, s;
     cin >> n >> m >> s;
     int u, v;
-    for (int i = 1; i <= n - 1; i++){
+    for (int i = 1; i <=  n - 1; i++){
         cin >> u >> v;
         addEdge(v, u);
+        addEdge(u, v);
     }
     for (int i = 1; i <= n; i++) fa[i] = i;
     int x, y;
@@ -54,6 +63,8 @@ int main(){
         addQ(y, x, i);
     }
     dfs(s, 0);
+    // for (int i = 1; i <= n; i++) cout << getfa(i) << ' ';
+    // cout << endl;
     for (int i = 1; i <= m; i++){
         cout << ans[i] << endl;
     }
