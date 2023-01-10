7faf55bba7641062cab61b527c1cff584517ec9b
oyzr1234 <ouyangzongrun@qq.com>
2023-01-10 12:35:09 +0800

    round8 A AC

--- other's_competition/round8/A.cpp
+++ other's_competition/round8/A.cpp
@@ -2,33 +2,34 @@
 #define int long long
 using namespace std;
 const int mod = 1e9 + 7;
-int count(int x){
-    int wei = floor(log10(x)) + 1;
-    int res = 0;
-    for (int i = 0; i < wei; i++){
-        int now = x / (int)(pow(10, i)) % 10;
-        if (now == 1) res++;
+int qpow(int x, int y){
+    int a = x;
+    int res = 1;
+    while(y){
+        if (y & 1){
+            res *= a;
+            res %= mod;
+        }
+        a *= a;
+        a %= mod;
+        y >>= 1;
     }
     return res;
 }
 signed main(){
-    freopen("lucky.in", "r", stdin);
-    freopen("lucky.out", "w", stdout);
+    // freopen("lucky.in", "r", stdin);
+    // freopen("lucky.out", "w", stdout);
     int T;
     cin >> T;
     cout << "My answer is:" << endl;
     while(T--){
         int n;
         cin >> n;
-        int minx = pow(10, n - 1);
-        int maxx = pow(10, n);
-        int ans = 0;
-        for (int i = minx; i < maxx; i++){
-            if (count(i) % 2 == 0) ans++;
-            ans %= mod;
-        }
+        int ans = 7 * qpow(8, n - 1) + 9 * qpow(10, n - 1);
+        ans *= qpow(2, mod - 2);
+        ans %= mod;
         if (n == 1) ans = 9;
-        cout << ans << endl;
+        printf("%lld\n", ans);
     }
     return 0;
 }
