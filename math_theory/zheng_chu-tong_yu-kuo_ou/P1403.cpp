6e6e95a05f31871e9545a945cb1e342f6e2929b2
oyzr1234 <ouyangzongrun@qq.com>
2023-03-03 20:35:52 +0800

    P1403 AC

---
+++ math_theory/zheng_chu-tong_yu-kuo_ou/P1403.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	long long ans = 0;
+	for (int i = 1; i <= n; i++){
+		ans += n / i;
+	}
+	cout << ans;
+	return 0;
+}
