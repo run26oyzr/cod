421b56ef9012025521b1ef8207414858686d6640
oyzr1234 <ouyangzongrun@qq.com>
2023-03-08 00:07:50 +0800

    P3811 加入证明

--- math_theory/su_shu-cheng_fa_ni_yuan-lu_ka_si/P3811.cpp
+++ math_theory/su_shu-cheng_fa_ni_yuan-lu_ka_si/P3811.cpp
@@ -6,6 +6,16 @@ int phi[maxn];
 signed main(){
     int n, p;
     cin >> n >> p;
+    /**
+     * 令p=ki+r(k=floor(p/i), r=p mod i)
+     * ki + r 同余 0 (mod p)
+     * 左右同乘 i^-1 * r^-1, 得：
+     * k * r^-1 + i^-1 同余 0 (mod p)
+     * 移向，得：
+     * i^-1 同余 -k * r^-1 (mod p)
+     * 代入，得：
+     * i^-1 同余 floor(-p/i) * (p%i)^-1 (mod p)
+    */
     //phi(i) = (-p/i)*phi(p%i)%p
     phi[1] = 1;
     for (int i = 2; i <= n; i++){
