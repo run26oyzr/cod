aa74e8167ab10f46f403c8d12f57c30f1d4333e4
oyzr1234 <ouyangzongrun@qq.com>
2023-03-25 12:43:18 +0800

    P1962 AC

---
+++ 数学/矩阵快速幂/P1962.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mod = 1e9 + 7;
+int n;
+struct Matrix{
+    static const int Size = 3;
+    int a[Size][Size];
+    Matrix(){memset(a, 0, sizeof(a));}
+    Matrix operator * (const Matrix &T)const{
+        Matrix res;
+        for (int i = 1; i < Size; i++){
+            for (int j = 1; j < Size; j++){
+                int tmp = 0LL;
+                for (int k = 1; k < Size; k++){
+                    tmp = (tmp + a[i][k] * T.a[k][j]) % mod;
+                }
+                res.a[i][j] = tmp;
+            } 
+        }
+        return res;
+    } 
+};
+Matrix qpow(Matrix A, long long b){
+    Matrix res;
+    for (int i = 1; i <= 2; i++)
+        res.a[i][i] = 1;
+    while(b){
+        if (b & 1) res = res * A;
+        A = A * A;
+        b >>= 1;
+    }
+    return res;
+}
+signed main(){
+    cin >> n;
+    Matrix A;
+    A.a[1][1] = 1;
+    A.a[2][1] = 0;
+    Matrix B;
+    B.a[1][1] = 1;
+    B.a[1][2] = 1;
+    B.a[2][1] = 1;
+    Matrix ans = A * qpow(B, n - 1);
+    cout << ans.a[1][1];
+    return 0;
+}
