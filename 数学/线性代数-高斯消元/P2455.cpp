16df6b630157a3742ef7d2a4d809ee7e19f95cb8
oyzr1234 <ouyangzongrun@qq.com>
2023-04-04 21:24:52 +0800

    P2455 AC

--- 数学/线性代数-高斯消元/P2455.cpp
+++ 数学/线性代数-高斯消元/P2455.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 55;
-const long double eps = 1e-4;
+const long double eps = 1e-10;
 long double a[maxn][maxn];
 long double x[maxn];
 long double tmp1[maxn], tmp2[maxn];
+int b[maxn];
 int main(){
 	int n;
 	cin >> n;
@@ -12,78 +13,67 @@ int main(){
 		for (int j = 1; j <= n + 1; j++)
 			cin >> a[i][j];
 	int curi = 1;
-	for(int i = 1; i <= n; i++){//消第几个元(主元为第i行第i列)
+	for(int i = 1; i <= n; i++){//消第几个元
 		//找主元  找i列中i行及以后行中的最大值 
-		int r = i; 
-		for (int j = i + 1; j <= n; j++)
-			if(fabs(a[r][curi]) < fabs(a[j][curi]))
+		int r = curi; 
+//		cout << i << ' ' << curi << endl;
+		for (int j = curi + 1; j <= n; j++)
+			if(fabs(a[r][i]) < fabs(a[j][i]))
 				r = j;
-		if (fabs(a[r][curi]) < eps){
-			//无解或多解，视a[r][n+1] 
+		if (fabs(a[r][i]) < eps){
+			//无解或多解，视a[r][n+1]
 			continue;
 		}
 		//交换
-		if (r != i)
+		if (r != curi)
 			for(int j = 1; j <= n + 1; j++)
-				swap(a[r][j], a[i][j]);
+				swap(a[r][j], a[curi][j]);
 		//归一化
-		double del = a[i][curi];
+		double del = a[curi][i];
 		for (int j = i; j <= n + 1; j++)
-			a[i][j] /= del; 
+			a[curi][j] /= del; 
 		//消元 
-		for (int j = i + 1; j <= n; j++){
-			del = a[j][curi];
-			for (int k = i; k <= n + 1; k++)
-				a[j][k] -= del * a[i][k];
+		for (int j = 1; j <= n; j++){
+			if (j != curi){
+				del = a[j][i];
+				for (int k = i; k <= n + 1; k++)
+					a[j][k] -= del * a[curi][k];
+			} 
 		}
+		b[curi] = i;
 		curi++;
 	}
-	//行阶梯矩阵 
 //	for (int i = 1; i <= n; i++){
 //		for (int j = 1; j <= n + 1; j++){
-//			printf("%.03Lf ", a[i][j]);
+//			printf("%.02Lf ", a[i][j]);
 //		}
 //		cout << endl;
 //	}
-	//回代 
 	bool flag = false;
-	x[n] = a[n][n + 1];
-    if (fabs(a[n][n]) < eps){
-        if (fabs(a[n][n + 1]) < eps){
-            cout << -1;
-            return 0;
-        }else flag = true;
-    }
-	for (int i = n - 1; i >= 1; i--){	
-		if (fabs(a[i][i]) < eps){
-			for (int j = n; j > i; j--){
-				a[i][n + 1] -= a[i][j] * x[j];
-				a[i][j] = 0;
-			}
+	for (int i = 1; i <= n; i++){
+		if (b[i] == 0){
 			if (fabs(a[i][n + 1]) >= eps){
 				cout << -1;
 				return 0;
-			}else flag = true;
-		}
-		x[i] = a[i][n + 1];
-		for (int j = n; j > i; j--){
-			x[i] -= a[i][j] * x[j];
-			a[i][j] = 0;
-		}
+			}
+			flag = true;
+			continue;
+		} 
+		if (fabs(a[i][b[i]]) < eps){
+			if (fabs(a[i][n + 1]) >= eps){
+//				cout << i << ' ' << b[i] << endl;
+				cout << -1;
+				return 0;
+			}
+			flag = true;
+		} 
 	}
-	//约化行阶梯矩阵 
-//	for (int i = 1; i <= n; i++){
-//		for (int j = 1; j <= n + 1; j++){
-//			printf("%.03Lf ", a[i][j]);
-//		}
-//		cout << endl;
-//	}
 	if (flag){
 		cout << 0;
 		return 0;
 	}
 	for (int i = 1; i <= n; i++){
-		printf("x%d=%.02Lf\n", i, x[i]);
+		printf("x%d=%.02Lf\n", i, a[i][n + 1]);
 	}
 	return 0;
 }
