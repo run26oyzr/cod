73b471cd1d3a50d64e114235dd5f6cbe89fb4c5c
oyzr1234 <ouyangzongrun@qq.com>
2023-09-13 19:58:15 +0800

    round 2023-08-23 B AC

--- 比赛/gdy出的比赛/2023-08-23/B.cpp
+++ 比赛/gdy出的比赛/2023-08-23/B.cpp
@@ -1,33 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e6 + 6;
+const int maxn = 1e6 + 6, maxlog = 21;
 int a[maxn];
-int lastpos[maxn], f[maxn];
+int lastpos[maxn], f[maxn][maxlog];
 int main(){
     int n, q;
-    cin >> n >> q;
+    scanf("%d %d", &n, &q);
     for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    int u, v;
+        scanf("%d", &a[i]);
+    for (int i = 1; i <= n; i++)
+        lastpos[i] = n + 1;
+    f[n + 1][0] = n + 1;
+    for (int i = n; i >= 1; i--){
+        f[i][0] = min(f[i + 1][0], lastpos[a[i]]);
+        lastpos[a[i]] = i;
+    }
+    for (int i = n; i >= 1; i--)
+        for (int k = 1; k < maxlog; k++){
+            if (f[i][k - 1] > n || f[i][k - 1] <= 0)
+                break;
+            f[i][k] = f[f[i][k - 1]][k - 1];
+        }
+    int l, r;
     while (q--){
-        cin >> u >> v;
-        if (u > v) 
-            swap(u, v);
-        if (u == v){
-            cout << '0' << endl;
-            continue;
+        scanf("%d %d", &l, &r);
+        if (l > r)
+            swap(l, r);
+        int u = l;
+        int ans = 2 * (r - l + 1) - 2;
+        for (int k = maxlog - 1; k >= 0; k--){
+            if (f[u][k] <= r && f[u][k] != 0){
+                u = f[u][k];
+                ans -= (1 << k);
+            }
         }
-        int ans = v - u;
-        for (int i = 1; i <= n; i++)
-            lastpos[i] = u - 1;
-        for (int i = u; i <= v; i++)
-            f[i] = 0x3f3f3f3f;
-        f[u - 1] = 0;
-        for (int i = u; i <= v; i++){
-            f[i] = min(f[i - 1] + 1, f[lastpos[a[i]]] + (i - lastpos[a[i]] - 1));
-            lastpos[a[i]] = i;
-        }
-        cout << ans + f[v] << endl;
+        printf("%d\n", ans);
     }
     return 0;
 }
