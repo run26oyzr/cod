111737f7cf195d97aaafac45e7fe06d11dfac374
oyzr1234 <ouyangzongrun@qq.com>
2023-10-05 14:27:27 +0800

    P8436 边双连通分量 AC

--- 团队题单/图论/图的遍历/P8436.cpp
+++ 团队题单/图论/图的遍历/P8436.cpp
@@ -1,47 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 5 * 1e5 + 5, maxm = 3 * 1e6 + 5;
-
+const int maxn = 5 * 1e5 + 5, maxm = 2 * 1e6 + 5;
 struct Node{
     int v, next;
-}t[maxm << 2];
-int tot = 0, h[maxn];
-int in[maxn], out[maxn];
+    int tag;
+    bool del;
+}t[maxm << 1];
+int tot = 1, h[maxn];
 void addEdge(int u, int v){
     t[++tot].v = v;
     t[tot].next = h[u];
     h[u] = tot;
-    in[v]++;
-    out[u]++;
 }
 
 int bccnum = 0;
-int st[maxn];
-int tp;
 vector<int> bcc[maxn];
-int dfn[maxn], low[maxn], dep;
-bool cut[maxn];
-void dfs(int u, int fa){
+int dfn[maxn], low[maxn], dep = 0;
+void tarjan(int u){
     dfn[u] = low[u] = ++dep;
-    int son = 0;
-    st[++tp] = u;
     for (int i = h[u]; i; i = t[i].next){
         int v = t[i].v;
-        if(v == fa) continue;
-        son++;
+        if (t[i].tag == -1)
+            continue;
+        t[i ^ 1].tag = -1;
         if (!dfn[v]){
-            dfs(v, u);
+            tarjan(v);
             low[u] = min(low[v], low[u]);
-            if (u != fa && low[v] > dfn[u]){
-                bccnum++;
-                while(1){
-                    int tmp = st[tp--];
-                    bcc[bccnum].push_back(tmp);
-                    if(tmp == v) break;
-                }
+            if (low[v] > dfn[u]){
+                t[i].del = t[i ^ 1].del = true;
             }
         }
         else low[u] = min(dfn[v], low[u]);
+    }
+}
+int vis[maxn];
+void dfs(int u){
+    vis[u] = 1;
+    bcc[bccnum].push_back(u);
+    for (int i = h[u]; i; i = t[i].next){
+        int v = t[i].v;
+        if (vis[v] || t[i].del)
+            continue;
+        dfs(v);
     }
 }
 int main(){
@@ -50,30 +50,27 @@ int main(){
     int x, y;
     while(m--){
         cin >> x >> y;
-        if (x == y) continue;
-        addEdge(x, y); addEdge(y, x);
+        if (x == y) 
+            continue;
+        addEdge(x, y);
+        addEdge(y, x);
     }
-    for (int i = 1; i <= n; i++){
-        if(!dfn[i]){
-            dfs(i, 0);
-            if (tp){
-                bccnum++;
-                while(tp){
-                    bcc[bccnum].push_back(st[tp--]);
-                }
-            }
+    for (int i = 1; i <= n; i++)
+        if (!dfn[i])
+            tarjan(i);
+    for (int i = 1; i <= n; i++)
+        if (!vis[i]){
+            ++bccnum;
+            dfs(i);
         }
-    }
     cout << bccnum << endl;
     for (int i = 1; i <= bccnum; i++){
         cout << bcc[i].size() << ' ';
-        while(!bcc[i].empty()){
+        while (!bcc[i].empty()){
             cout << bcc[i].back() << ' ';
             bcc[i].pop_back();
         }
-        
         cout << endl;
     }
-    system("pause");
     return 0;
 }
